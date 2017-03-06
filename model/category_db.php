a19032910600cceb5b1e61b43176d698aece6484
mounika guntupalli <mg577@afsconnect1.njit.edu>
2017-03-05 22:07:13 -0500

    Adding commands

--- model/category_db.php
+++ model/category_db.php
@@ -1,16 +1,15 @@
 <?php
 function get_categories() {
     global $db;
-    $query = 'SELECT * FROM categories
+    $query = 'SELECT * FROM categories_guitar1
               ORDER BY categoryID';
     $statement = $db->prepare($query);
     $statement->execute();
     return $statement;    
 }
-
 function get_category_name($category_id) {
     global $db;
-    $query = 'SELECT * FROM categories
+    $query = 'SELECT * FROM categories_guitar1
               WHERE categoryID = :category_id';    
     $statement = $db->prepare($query);
     $statement->bindValue(':category_id', $category_id);
@@ -19,5 +18,25 @@ function get_category_name($category_id) {
     $statement->closeCursor();    
     $category_name = $category['categoryName'];
     return $category_name;
+}
+function add_category($categoryName){
+    global $db;
+    $query = 'INSERT INTO categories_guitar1
+              (categoryName)
+	      VALUES
+	      (:categoryName)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':categoryName', $categoryName);
+    $statement->execute();
+    $statement->closeCursor();
+}
+function delete_category($categoryID){
+    global $db;
+    $query = 'DELETE FROM categories_guitar1
+              WHERE categoryID = :categoryID';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':categoryID', $categoryID);
+    $statement->execute();
+    $statement->closeCursor();
 }
 ?>
