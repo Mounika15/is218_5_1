765f9d03adc505f81e21e8ec1061c52851ed5b00
mounika guntupalli <mg577@afsconnect1.njit.edu>
2017-03-05 22:43:29 -0500

    Changed name

--- product_manager/category_list.php
+++ product_manager/category_list.php
@@ -6,11 +6,25 @@
         <tr>
             <th>Name</th>
             <th>&nbsp;</th>
-        </tr>        
-        <!-- add category rows here -->
+        </tr>
+        <?php foreach ($categories as $category) : ?>
+        <tr>
+	<td><?php echo $category['categoryName']; ?></td>
+	<td><form action="." method="post">
+	 <input type="hidden" name="action"
+	  value="delete_category">
+	 <input type="hidden" name="categoryID"
+	  value="<?php echo $category['categoryID']; ?>">
+	 <input type="submit" value="Delete">
+	 </form></td>
+	 </tr>
+	 <?php endforeach; ?>
+	<!-- add category rows here -->
     </table>
 
     <h2>Add Category</h2>
+     <p><a href="index.php?action=show_add_category_form">Add Category</a></p>
+
     <!-- add code for form here -->
 
     <p><a href="index.php?action=list_products">List Products</a></p>
